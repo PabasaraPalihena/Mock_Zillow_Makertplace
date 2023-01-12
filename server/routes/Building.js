185b3870df5b757daa870cfede57212dbfdfb2cd
PabasaraPalihena <pabapalihena@gmail.com>
2023-01-13 00:41:27 +0530

    created the routes

---
+++ server/routes/Building.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const {
+  addRealEstate,
+  getallRealEstate,
+  getRealEstate,
+  updateRealEstate,
+  deleteRealEstate,
+} = require("../controllers/Building");
+
+router.get("/", getallRealEstate); //get all Estates
+router.get("/:id", getRealEstate); //get Estate using id
+router.post("/", addRealEstate); //add a realestate
+router.put("/:id", updateRealEstate); //update estate details
+router.delete("/:id", deleteRealEstate); //delete the estate
+
+module.exports = router;
